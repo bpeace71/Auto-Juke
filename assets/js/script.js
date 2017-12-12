7a880b245c8d8a8af9e5310c00ad18add6e7ca16
Co-Su <thisusernameisnottaken@gmail.com>
2017-12-11 18:46:44 -0800

    Weather API and ZIP API are linked and now convert Zip Codes to current temp, pushing out a variable for other APIs to latch onto. -C

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,38 @@
-function findLocationWithZip() {
 
-  var zipCode = "30080"
-    var zipKey = "zItY5bAzQcwNyA8gRZbiWmh1VHPF6bGkr9IdtR7ffhZFZclIYe8NmieX5OuGkjCl"
-    var latitude = "33.748995"
-    var longitude = "-84.387982"
-    var queryZipURL = "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/" + zipKey + "/info.json/" + zipCode + "/degrees"
-    
+
+
+
+var zipCode = document.getElementById("inputbox").value;
+//var latitude = "#"
+//var longitude = "#"
+
+$("button").on("click", function findLocationWithZip() {
+    event.preventDefault();   
+    var zipCode = document.getElementById("inputbox").value;
+    var zipKey = "zItY5bAzQcwNyA8gRZbiWmh1VHPF6bGkr9IdtR7ffhZFZclIYe8NmieX5OuGkjCl";
+    var weatherKey = "23a22b2c326490bd56054c01f0eb55d2";
+
+    var queryZipURL = "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/" + zipKey + "/info.json/" + zipCode + "/degrees";
+
     $.ajax({
       url: queryZipURL,
       method: "GET"
-    }).done(function(response) {
-      console.log(response)
-    });
-  }
-
-findLocationWithZip()
-
-var zipCode = "30080"
-var latitude = "33.748995"
-var longitude = "-84.387982"
-
-function searchWeather() {
-
-    var weatherKey = "988e9c01c043c5977d3b37bfaad72e6d"
-    var queryURLTwo = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + weatherKey + "/" + latitude +"," + longitude + ""
-    
+    }).done(function(zipResponse) {
+      console.log(zipResponse);
+      console.log(zipResponse.city);
+      console.log(zipResponse.lat);
+      console.log(zipResponse.lng);
+      latitude = (zipResponse.lat);
+      longitude = (zipResponse.lng);
+//Dark Sky converts latitude and Longitude and spits out temperature.
+    var queryURLTwo = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + weatherKey + "/" + latitude +"," + longitude + "";
     $.ajax({
       url: queryURLTwo,
       method: "GET"
-    }).done(function(response) {
-      console.log(response)
+    }).done(function(darkSkyResponse) {
+      console.log(darkSkyResponse)
+      console.log(darkSkyResponse.currently.temperature);
+      currentTemp = (darkSkyResponse.currently.temperature)
+      });
     });
-  }
-
-searchWeather()
+  });
